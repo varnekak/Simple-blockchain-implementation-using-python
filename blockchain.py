76a7f75bdf807cdb46e4da6bbbdf39ab6b3b0697
varnekak <varnekak2005@gmail.com>
2026-06-07 21:39:07 +0530

    vfhnd

--- blockchain.py
+++ blockchain.py
@@ -2,59 +2,10 @@ import hashlib
 import json
 from time import time 
 from uuid import uuid4
-from flask import Flask
+from flask import Flask,jsonify, request
 from textwrap import dedent
 
 
-# instatiate our Node
-app = Flask(__name__)
-
-# generate a globallay unique adress for this node 
-node_identifier= str(uuid4()).replace('-','')
-
-# instantiate a Blockchain
-blockchain =  Blockchain()
-
-@app.route('/mine',methods=['GET'])
-def mine():
-     return "We ll mine a new block"
-
-
-@app.route('/chain', methods=['GET'])
-def full_chain():
-     response = {
-          'chain': blockchain.chain;
-          'length': len(blockchain.chain)
-     }
-     return jsonify(response), 200
-
-
-@app.route('transactions/new', methods=['POST'])
-def new_transaction():
-     values = request.get_json()
-
-     required=['sender','recipient','amount']
-     if not all(k in values for k in required):
-          return 'Missing values', 400
-     
-     index = blockchain.new_transaction(
-          values['sender'],
-          values['recipient'],
-          values['amount']
-     )
-     response = {
-          'message': f'Transactions added to Block {index}'
-     }
-
-     return jsonify(response),201
-
-@app.route('/mine', methods=['GET'])
-def mine():
-     
-     last_block = blockchain.last_block
-     last_proof = last_block['proof']
-
-     proof = blockchain.proof_of_
 
 
 
@@ -129,13 +80,97 @@ class Blockchain(object):
         return self.chain[-1]
     
     def proof_of_work(self, last_proof):
+           """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
          proof =0
          while self.valid_proof(last_proof, proof) is False:
               proof +=1
-        return proof 
+         return proof 
     
     @staticmethod 
     def valid_proof(last_proof, proof):
+         """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
          guess = f'{last_proof}{proof}'.encode()
-         guess_hash =  hashlib.sha256(guess).hexadigest()
+         guess_hash =  hashlib.sha256(guess).hexdigest()
          return guess_hash[:4] == "0000"
+    
+    # instatiate our Node
+app = Flask(__name__)
+
+# generate a globallay unique adress for this node 
+node_identifier= str(uuid4()).replace('-','')
+
+# instantiate a Blockchain
+blockchain =  Blockchain()
+
+@app.route('/mine',methods=['GET'])
+def mine():
+     return "We ll mine a new block"
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+     response = {
+          'chain': blockchain.chain,
+          'length': len(blockchain.chain)
+     }#dictionary
+     return jsonify(response), 200
+
+if __name__ == '__main__':
+     app.run(host='0.0.0.0', port=5000)
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+     values = request.get_json()
+
+     required=['sender','recipient','amount']
+     if not all(k in values for k in required):
+          return 'Missing values', 400
+     
+     index = blockchain.new_transaction(
+          values['sender'],
+          values['recipient'],
+          values['amount']
+     )
+     response = {
+          'message': f'Transactions added to Block {index}'
+     }
+
+     return jsonify(response),201
+
+@app.route('/mine', methods=['GET'])
+def mine():
+      # We run the proof of work algorithm to get the next proof...
+     last_block = blockchain.last_block
+     last_proof = last_block['proof']
+
+     proof = blockchain.proof_of_work(last_proof)
+
+      # We must receive a reward for finding the proof.
+    # The sender is "0" to signify that this node has mined a new coin.
+
+    blockchain.new_transaction(
+         sender="0",
+         recipient=node_identifier,
+         amount=1,
+    )
+   # Forge the new Block by adding it to the chain
+
+   response = {
+        'message':"New Block Forged",
+        'index':block['index'],
+        'transactions': block['transactions'],
+        'proof':block['proof'],
+        'previous_hash': block['previous_hash'],
+   }
+    return jsonify(response), 200
